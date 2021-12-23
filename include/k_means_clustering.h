4e5b811f14962b82d409bb1f50a8b4b057070d08
exTerEX <30706568+exTerEX@users.noreply.github.com>
2021-12-23 10:07:37 +0100

    Reflected changes to dimention in python and header files

--- include/k_means_clustering.h
+++ include/k_means_clustering.h
@@ -1,13 +1,27 @@
 #include <stdlib.h>
 
-typedef struct observation {
-    double x, y;
-    size_t group;
-} observation_t;
+typedef struct observation_2d {
+  double x, y;
+  size_t group;
+} observation_t, observation_2d_t;
 
-typedef struct cluster {
-    double x, y;
-    size_t count;
-} cluster_t;
+typedef struct observation_3d {
+  double x, y, z;
+  size_t group;
+} observation_3d_t;
+
+typedef struct cluster_2d {
+  double x, y;
+  size_t count;
+} cluster_t, cluster_2d_t;
+
+typedef struct cluster_3d {
+  double x, y, z;
+  size_t count;
+} cluster_3d_t;
 
 cluster_t *k_means(observation_t observations[], size_t size, size_t k);
+
+cluster_2d_t *k_means_2d(observation_2d_t observations[], size_t size, size_t k);
+
+cluster_3d_t *k_means_3d(observation_3d_t observations[], size_t size, size_t k);
--- kmeans/k_means_clustering.py
+++ kmeans/k_means_clustering.py
@@ -5,23 +5,31 @@ from typing import *
 from numpy import array, cos, ndarray, pi, random, sin, zeros
 
 try:
-    lib = CDLL(f"kmeans_{sys.platform}.so")
+    lib = CDLL(f"build/kmeans_{sys.platform}.so")
 except BaseException:
     print(f"OS {sys.platform} not recognized")
 
 
-class observation(Structure):
+class observation_2d(Structure):
     _fields_ = [("x", c_double), ("y", c_double), ("group", c_size_t)]
 
 
-class cluster(Structure):
+class observation_3d(Structure):
+    _fields_ = [("x", c_double), ("y", c_double), ("z", c_double), ("group", c_size_t)]
+
+
+class cluster_2d(Structure):
     _fields_ = [("x", c_double), ("y", c_double), ("count", c_size_t)]
 
 
-lib.k_means.restype = POINTER(cluster)
+class cluster_3d(Structure):
+    _fields_ = [("x", c_double), ("y", c_double), ("z", c_double), ("count", c_size_t)]
 
 
-def k_means(observations: ndarray, k: Optional[int] = 5) -> Tuple[ndarray, ndarray]:
+lib.k_means_2d.restype = POINTER(cluster_2d)
+
+
+def k_means_2d(observations: ndarray, k: Optional[int] = 5) -> Tuple[ndarray, ndarray]:
     """Partition observations into k clusters.
 
     Parameters
@@ -45,7 +53,7 @@ def k_means(observations: ndarray, k: Optional[int] = 5) -> Tuple[ndarray, ndarr
     Examples
     -------
     >>> observations = [[6, 1], [-4, -4], [1, -7], [9, -2], [6, -6]]
-    >>> center, count = k_means(observations, k=2)
+    >>> center, count = k_means_2d(observations, k=2)
     >>> center
     [[-4, -4
        5, -3]]
@@ -62,17 +70,17 @@ def k_means(observations: ndarray, k: Optional[int] = 5) -> Tuple[ndarray, ndarr
         raise ValueError(
             "Provided array should contain ((x, y), ) observations.")
 
-    # Find observation length
+    # Find observation_2d length
     n = observations.shape[-1]
 
     # Create a Python list of observations
-    py_observations_list = map(observation, *observations)
+    py_observations_list = map(observation_2d, *observations)
 
     # Convert the Python list into a c-array
-    c_observations_array = (observation * n)(*py_observations_list)
+    c_observations_array = (observation_2d * n)(*py_observations_list)
 
-    # Get c-array of cluster
-    c_clusters_array = lib.k_means(
+    # Get c-array of cluster_2d
+    c_clusters_array = lib.k_means_2d(
         c_observations_array, c_size_t(n), c_size_t(k))
 
     # Convert c-array of clusters into a python list
@@ -91,6 +99,44 @@ def k_means(observations: ndarray, k: Optional[int] = 5) -> Tuple[ndarray, ndarr
     return (center, count)
 
 
+def k_means(observations: ndarray, k: Optional[int] = 5) -> Tuple[ndarray, ndarray]:
+    """Partition observations into k clusters.
+
+    Parameters
+    ----------
+    observations : ndarray, `shape (N, 2)` or `shape (N, 3)`
+        An array of observations (x, y) to be clustered.
+
+        Data should be provided as:
+        `[(x, y), (x, y), (x, y), ...]`
+        or
+        `[(x, y, z), (x, y, z), (x, y, z), ...]`
+
+    k : int, optional
+        Amount of clusters to partition observations into, by default 5
+
+    Returns
+    -------
+    center : ndarray, `shape (k, 2)` or `shape (k, 3)`
+        An array of positions to center of each cluster.
+
+    count : ndarray, `shape (k, )`
+        Array of counts of datapoints closest to the center of its cluster.
+
+    Examples
+    -------
+    >>> observations = [[6, 1], [-4, -4], [1, -7], [9, -2], [6, -6]]
+    >>> center, count = k_means_2d(observations, k=2)
+    >>> center
+    [[-4, -4
+       5, -3]]
+    >>> count
+    [1, 4]
+    """
+    # TODO: Check if input is 2d or 3d. Use appropriate function.
+    return k_means_2d(observations, k)
+
+
 if __name__ == "__main__":
     random.seed(1234)
 
@@ -103,7 +149,7 @@ if __name__ == "__main__":
 
     print(f"Observations:\n{df[0:5]}\n...\n\nshape {len(df), len(df[0])}\n")
 
-    centers, count = k_means(df, 3)
+    centers, count = k_means_2d(df, 3)
 
     print(f"Centers:\n{centers}\n")
     print(f"Count:\n{count}")
