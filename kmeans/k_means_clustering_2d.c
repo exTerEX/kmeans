14c2ad6b5370c2a2136730c304215c2c390c2095
exTerEX <30706568+exTerEX@users.noreply.github.com>
2021-12-23 10:05:44 +0100

    Renamed file, and reflected changes to dimention changes

---
+++ kmeans/k_means_clustering_2d.c
@@ -0,0 +1,112 @@
+#include "k_means_clustering.h"
+#include <float.h>
+#include <string.h>
+
+#ifdef __STDC_LIB_EXT1__
+#define E 1
+#endif
+#ifndef __STDC_LIB_EXT1__
+#define E 2
+#endif
+
+#define MIN_ERROR_DENOMINATOR_SIZE 10000
+
+size_t calculate_nearst(observation_2d_t *observation, cluster_2d_t clusters[], size_t k) {
+  double min_distance = DBL_MAX;
+  double distance = 0;
+
+  size_t nearest_index = -1;
+
+  for (size_t index = 0; index < k; index++) {
+    distance = (clusters[index].x - observation->x) * (clusters[index].x - observation->x) +
+               (clusters[index].y - observation->y) * (clusters[index].y - observation->y);
+
+    if (distance < min_distance) {
+      min_distance = distance;
+      nearest_index = index;
+    }
+  }
+  return nearest_index;
+}
+
+void calculate_centroid(observation_2d_t observations[], size_t size, cluster_2d_t *centroid) {
+  centroid->x = 0;
+  centroid->y = 0;
+  centroid->count = size;
+
+  for (size_t index = 0; index < size; index++) {
+    centroid->x += observations[index].x;
+    centroid->y += observations[index].y;
+    observations[index].group = 0;
+  }
+
+  centroid->x /= centroid->count;
+  centroid->y /= centroid->count;
+}
+
+cluster_2d_t *k_means_2d(observation_2d_t observations[], size_t size, size_t k) {
+  cluster_2d_t *clusters = NULL;
+
+  if (k <= 1) {
+    clusters = (cluster_2d_t *)malloc(sizeof(cluster_2d_t));
+    memset(clusters, 0, sizeof(cluster_2d_t));
+    calculate_centroid(observations, size, clusters);
+  } else if (k < size) {
+    clusters = malloc(sizeof(cluster_2d_t) * k);
+    memset(clusters, 0, k * sizeof(cluster_2d_t));
+
+    for (size_t index = 0; index < size; index++) {
+      observations[index].group = rand() % k;
+    }
+
+    size_t changed = 0;
+    size_t min_error = size / MIN_ERROR_DENOMINATOR_SIZE;
+
+    size_t temp = 0;
+
+    do {
+      for (size_t index = 0; index < k; index++) {
+        clusters[index].x = 0;
+        clusters[index].y = 0;
+        clusters[index].count = 0;
+      }
+
+      for (size_t index = 0; index < size; index++) {
+        temp = observations[index].group;
+        clusters[temp].x += observations[index].x;
+        clusters[temp].y += observations[index].y;
+        clusters[temp].count++;
+      }
+
+      for (size_t index = 0; index < k; index++) {
+        clusters[index].x /= clusters[index].count;
+        clusters[index].y /= clusters[index].count;
+      }
+
+      changed = 0;
+      for (size_t index = 0; index < size; index++) {
+        temp = calculate_nearst(observations + index, clusters, k);
+
+        if (temp != observations[index].group) {
+          changed++;
+          observations[index].group = temp;
+        }
+      }
+    } while (changed > min_error);
+  } else {
+    clusters = (cluster_2d_t *)malloc(sizeof(cluster_2d_t) * k);
+    memset(clusters, 0, k * sizeof(cluster_2d_t));
+
+    for (size_t index = 0; index < size; index++) {
+      clusters[index].x = observations[index].x;
+      clusters[index].y = observations[index].y;
+      clusters[index].count = 1;
+      observations[index].group = index;
+    }
+  }
+  return clusters;
+}
+
+cluster_t *k_means(observation_t observations[], size_t size, size_t k) {
+  return k_means_2d(observations, size, k);
+}
