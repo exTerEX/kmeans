2a2ad58a4f65296667017590a5002584aa25d78e
exTerEX <30706568+exTerEX@users.noreply.github.com>
2021-12-26 13:39:54 +0100

    Added 3d support in python script

--- lib/python/kmeans/core/kmeans.py
+++ lib/python/kmeans/core/kmeans.py
@@ -1,10 +1,10 @@
 from ctypes import *
 from typing import *
 
-from numpy import array, cos, ndarray, pi, random, sin, zeros
+from numpy import array, cos, ndarray, pi, random, sin, zeros, tan
 
 try:
-    lib = CDLL(f"libkmeans.so")
+    lib = CDLL(f"./libkmeans.so")
 except Exception as E:
     print(f"Cannot load DLL")
     print(E)
@@ -67,8 +67,7 @@ def k_means_2d(observations: ndarray, k: Optional[int] = 5) -> Tuple[ndarray, nd
     if observations.shape[-1] == 2:
         observations = observations.T
     else:
-        raise ValueError(
-            "Provided array should contain ((x, y), ) observations.")
+        raise ValueError("Provided array should contain ((x, y), ) observations.")
 
     # Find observation_2d length
     n = observations.shape[-1]
@@ -99,7 +98,80 @@ def k_means_2d(observations: ndarray, k: Optional[int] = 5) -> Tuple[ndarray, nd
     return (center, count)
 
 
-def k_means(observations: ndarray, k: Optional[int] = 5) -> Tuple[ndarray, ndarray]:
+lib.k_means_3d.restype = POINTER(cluster_3d)
+
+
+def k_means_3d(observations: ndarray, k: Optional[int] = 5) -> Tuple[ndarray, ndarray]:
+    """Partition observations into k clusters.
+
+    Parameters
+    ----------
+    observations : ndarray, `shape (N, 3)`
+        An array of observations (x, y) to be clustered.
+
+        Data should be provided as: `[(x, y, z), (x, y, z), (x, y, z), ...]`
+
+    k : int, optional
+        Amount of clusters to partition observations into, by default 5
+
+    Returns
+    -------
+    center : ndarray, `shape (k, 3)`
+        An array of positions to center of each cluster.
+
+    count : ndarray, `shape (k, )`
+        Array of counts of datapoints closest to the center of its cluster.
+
+    Examples
+    -------
+    >>> observations = [[6, 1, 3], [-4, -4, -4], [1, -7, 7], [9, -2, 1], [6, -6, 6]]
+    >>> center, count = k_means_3d(observations, k=2)
+    >>> center
+    [[ -0.35830777  -7.41219447 201.90265473]
+     [  1.83808572  -5.86460671 -28.00696338]
+     [ -0.81562641  -1.20418037   1.60364838]]
+    >>> count
+    [2, 3]
+    """
+    if not isinstance(observations, ndarray):
+        raise TypeError("Observations must be a ndarray.")
+
+    # Fix orientation on data
+    if observations.shape[-1] == 3:
+        observations = observations.T
+    else:
+        raise ValueError("Provided array should contain ((x, y, z), ) observations.")
+
+    # Find observation_3d length
+    n = observations.shape[-1]
+
+    # Create a Python list of observations
+    py_observations_list = map(observation_3d, *observations)
+
+    # Convert the Python list into a c-array
+    c_observations_array = (observation_3d * n)(*py_observations_list)
+
+    # Get c-array of cluster_2d
+    c_clusters_array = lib.k_means_3d(c_observations_array, c_size_t(n), c_size_t(k))
+
+    # Convert c-array of clusters into a python list
+    py_clusters_list = [c_clusters_array[index] for index in range(k)]
+
+    # Split clusters
+    center = zeros([k, 3], dtype=observations.dtype)
+    count = zeros(k, dtype=int)
+
+    for index, cluster_object in enumerate(py_clusters_list):
+        center[index][0] = cluster_object.x
+        center[index][1] = cluster_object.y
+        center[index][2] = cluster_object.z
+        count[index] = cluster_object.count
+
+    # Pack into DataFrame and return
+    return (center, count)
+
+
+def kmeans(observations: ndarray, k: Optional[int] = 5) -> Tuple[ndarray, ndarray]:
     """Partition observations into k clusters.
 
     Parameters
@@ -133,8 +205,16 @@ def k_means(observations: ndarray, k: Optional[int] = 5) -> Tuple[ndarray, ndarr
     >>> count
     [1, 4]
     """
-    # TODO: Check if input is 2d or 3d. Use appropriate function.
-    return k_means_2d(observations, k)
+
+    if not isinstance(observations, ndarray):
+        raise TypeError("Observations must be a ndarray.")
+
+    if observations.shape[-1] == 3:
+        return k_means_3d(observations, k)
+    elif observations.shape[-1] == 2:
+        return k_means_2d(observations, k)
+    else:
+        pass
 
 
 if __name__ == "__main__":
@@ -144,12 +224,19 @@ if __name__ == "__main__":
 
     x = 10 * rand_list * cos(2 * pi * rand_list)
     y = 10 * rand_list * sin(2 * pi * rand_list)
+    z = 10 * rand_list * tan(2 * pi * rand_list)
 
-    df = array([x, y]).T
+    df = array([x, y, z]).T
 
     print(f"Observations:\n{df[0:5]}\n...\n\nshape {len(df), len(df[0])}\n")
 
-    centers, count = k_means_2d(df, 3)
+    centers, count = kmeans(df, 3)
+
+    print(f"Centers:\n{centers}\n")
+    print(f"Count:\n{count}")
+
+    observations = [[6, 1], [-4, -4], [1, -7], [9, -2], [6, -6]]
+    center, count = k_means_2d(array(observations), k=2)
 
     print(f"Centers:\n{centers}\n")
     print(f"Count:\n{count}")
