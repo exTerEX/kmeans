14d59e45ead23977d749410093d223a272af2f28
exTerEX <30706568+exTerEX@users.noreply.github.com>
2022-01-11 23:09:20 +0100

    Updated setup.py with cython

--- extras/python-bindings/setup.py
+++ extras/python-bindings/setup.py
@@ -1,64 +1,60 @@
 #!/usr/bin/env python
-import os
-from pathlib import Path
+import setuptools
+import Cython
 
-from setuptools import Extension, find_packages, setup
-from setuptools.command.build_ext import build_ext
-
-
-class CMakeExtension(Extension):
-    def __init__(self, name):
-        super().__init__(name, sources=[])
-
-
-class build_ext(build_ext):
-    def run(self):
-        for ext in self.extensions:
-            self.build_cmake(ext)
-        super().run()
-
-    def build_cmake(self, ext):
-        cwd = Path().absolute()
-
-        build_temp = Path(self.build_temp)
-        build_temp.mkdir(parents=True, exist_ok=True)
-        extdir = Path(self.get_ext_fullpath(ext.name))
-        extdir.mkdir(parents=True, exist_ok=True)
-
-        config = "Debug" if self.debug else "Release"
-        cmake_args = [
-            "-DCMAKE_LIBRARY_OUTPUT_DIRECTORY=" + str(extdir.parent.absolute()),
-            "-DCMAKE_BUILD_TYPE=" + config
-        ]
-
-        build_args = ["--config", config, "--", "-j4"]
-
-        os.chdir(str(build_temp))
-        self.spawn(["cmake", str(cwd)] + cmake_args)
-        if not self.dry_run:
-            self.spawn(["cmake", "--build", "."] + build_args)
-        os.chdir(str(cwd))
-
+kmeans_extension = setuptools.Extension(
+    "kmeans.kmeans_ext",
+    language="c",
+    sources=["src/kmeans/kmeans_ext.pyx"],
+    libraries=["kmeans"]
+)
 
 with open("README.md", "r", encoding="utf-8") as file:
     long_description = file.read()
 
+with open("requirements/requirements-setup.txt", "r", encoding="utf-8") as file:
+    setup_requires = file.read()
+
+with open("requirements/requirements.txt", "r", encoding="utf-8") as file:
+    install_requires = file.read()
 
 if __name__ == "__main__":
-    setup(
-        name="kmeans",
-        version="0.1.0",
-        license="MIT",
-        author="Andreas Sagen",
-        description="A kmeans clustering algorithm implemented in C",
+    setuptools.setup(
+        name="KMeans",
+        version="0.0.1",
+        description="Wrapper to native \"kmeans\" library",
         long_description=long_description,
         long_description_content_type="text/markdown",
-        packages=find_packages(where="lib/python/kmeans"),
+        author="Andreas Sagen",
+        author_email="developer@sagen.io",
+        url="https://github.com/exterex/kmeans/extras/python-bindings",
+        packages=["kmeans"],
+        ext_modules=Cython.Build.cythonize(
+            kmeans_extension,
+            language_level=3.6,
+            compiler_directives={"linetrace": True}
+        ),
+        package_dir={"": "src"},
         package_data={
-            "kmeans": ["libkmeans.so"]
+            "kmeans": ["kmeans_ext.pxd", "c_kmeans.pxd"]
         },
-        ext_modules=[CMakeExtension("kmeans/libkmeans")],
-        cmdclass={"build_ext": build_ext},
-        python_requires=">=3.6",
-        install_requires=["numpy"]
+        include_package_data=True,
+        setup_requires=setup_requires.splitlines(),
+        zip_safe=False,
+        install_requires=install_requires.splitlines(),
+        keywords=[
+            "Clustering",
+            "K-Means"
+        ],
+        classifiers=[
+            "Development Status :: 3 - Alpha",
+            "License :: OSI Approved :: MIT License",
+            "Natural Language :: English",
+            "Operating System :: OS Independent",
+            "Programming Language :: Cython",
+            "Programming Language :: C",
+            "Programming Language :: Python :: Implementation :: CPython",
+            "Topic :: Software Development :: Libraries",
+        ],
+        license="MIT"
     )
